b2fc220f89bf7dd4b3183de6f0948f8bd83dfef9
Rupali <rupali.comp@gmail.com>
2025-07-25 15:45:45 +0530

    java Fun Programming to Github

---
+++ eclipse-workspace/JavaFun/src/S/welcomeJava.java
@@ -0,0 +1,19 @@
+package S;
+
+ class welcomeJava{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome To Jva Batch  "+ "  Welcome Isha");
+		
+		int a=10;
+		long b;
+		b=a;
+		a=(int) b;
+		
+		
+
+	}
+
+}
